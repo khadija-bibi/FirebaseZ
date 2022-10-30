3859e86743c175063635435490110e17db904477
khadija-bibi <sub99way1122@gmail.com>
2022-10-30 18:16:20 +0500

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,130 @@
-import 'package:demoapp/ui/resetpass.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  runApp(MyApp());
+  await Firebase.initializeApp();
+  runApp(MaterialApp(home: ResetPass()));
 }
 
-class MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
+class ResetPass extends StatelessWidget {
+  ResetPass({super.key});
+  final controller=TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: FutureBuilder(
-        future: _fbApp,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print('You have an error! ${snapshot.error.toString()}');
-            return Text('Something went wrong!');
-          } else if (snapshot.hasData) {
-            return Home();
-          } else
-            Center(
-              child: CircularProgressIndicator(),
-            );
-        },
-      ),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  Home({Key? key}) : super(key: key);
-
-  hexColor(String colorhexcode) {
-    String colornew = '0xff' + colorhexcode;
-    colornew = colornew.replaceAll('#', '');
-    int colorint = int.parse(colornew);
-    return colorint;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(hexColor('#FDE88D')),
       appBar: AppBar(
-        title: Text('Sign in'),
         backgroundColor: Color(hexColor('#819F84')),
+        title: const Text("Reset your password",
+            style: TextStyle(color: Colors.white)),
       ),
       body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ResetPass()));
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(13.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(color: Color(hexColor('#FDA8A5'))),
-                    color: Color(hexColor('#FDA8A5')),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(50.0),
+                  child: Image.asset('images/img.png'),
+                ),
+                const Text(
+                  "Forgot your Password",
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
-                  child: const Center(
-                    child: Text("RESET PASSWORD",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Enter your email id to\nretrieve your password",
+                  style: TextStyle(
+                    fontStyle: FontStyle.normal,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: Colors.white10),
+                      color: Colors.white,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 30.0),
+                      child: TextField(
+                        // controller: controller,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email",
+                        ),),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      final email = controller.text;
+
+                      createUser(email: email);
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                        padding: EdgeInsets.all(13.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Color(hexColor('#FDA8A5')),
+                            border:
+                            Border.all(color: Color(hexColor('#FDA8A5')))),
+
+                        child: const Text(
+                          "ENTER",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
                         )),
                   ),
                 ),
-              ),
+              ],
             ),
-          ]),
+          ),
         ),
       ),
     );
+  }
+
+
+  hexColor(String colorhexcode) {
+    String colornew = '0xff$colorhexcode';
+    colornew = colornew.replaceAll('#', '');
+    int colorint = int.parse(colornew);
+    return colorint;
+  }
+
+  Future createUser({required String email}) async{
+    final docUser=FirebaseFirestore.instance.collection('users').doc(' my-id');
+final json= {
+
+
+    'email':email,
+    };
+
+
+    await docUser.set(json);
   }
 }
